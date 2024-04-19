4db225b54b180924ced0e13d0748c42c411555c6
Maria Fernanda Palencia <39041288+mafer23@users.noreply.github.com>
2024-04-19 17:50:17 -0500

    prueba Crear extension
    Jose hecha un ojo

---
+++ data.js
@@ -0,0 +1,14 @@
+import {extract} from '@extractus/article-extractor'
+
+
+const article = await extract('https://www.elespectador.com/colombia/mas-regiones/danos-por-derrumbes-quien-responde-article-28830/')
+
+
+const message = document.querySelector('h1')
+
+const boton = document.querySelector('#change-message')
+
+
+boton.addEventListener('click', () =>{
+    message.textContent = "nuevo mensaje"
+})
