2a5088f19a765732b30664e36a5cbec4f8dd00aa
Maria Fernanda Palencia <39041288+mafer23@users.noreply.github.com>
2024-04-19 17:53:39 -0500

    Update data.js

--- data.js
+++ data.js
@@ -4,11 +4,11 @@ import {extract} from '@extractus/article-extractor'
 const article = await extract('https://www.elespectador.com/colombia/mas-regiones/danos-por-derrumbes-quien-responde-article-28830/')
 
 
-const message = document.querySelector('h1')
+//const message = document.querySelector('h1')
 
-const boton = document.querySelector('#change-message')
+//const boton = document.querySelector('#change-message')
 
 
-boton.addEventListener('click', () =>{
-    message.textContent = "nuevo mensaje"
-})
+//boton.addEventListener('click', () =>{
+  //  message.textContent = "nuevo mensaje"
+//})
